538d632dc5c1a5690384b26abf6d3857493aec24
Anirudh Salaria <109616328+anirudhsalaria@users.noreply.github.com>
2023-05-28 14:02:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar title = "TextGo" />
+      
+    </>
+  );
+}
+
+export default App;
